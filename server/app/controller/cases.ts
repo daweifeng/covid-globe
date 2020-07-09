2007756917be3b1644c073aeb6bb89053c637462
Dawei Feng <imdavidf@gmail.com>
2020-07-09 00:38:41 -0700

    Add Cases route and controller

--- server/app/controller/cases.ts
+++ server/app/controller/cases.ts
@@ -3,6 +3,6 @@ import { Controller } from 'egg';
 export default class CasesController extends Controller {
   public async confirmed() {
     const { ctx } = this;
-    ctx.body = "confirmed caess route"
+    ctx.body = 'confirmed caess route';
   }
 }
--- server/app/service/Cases.ts
+++ server/app/service/Cases.ts
@@ -1,4 +1,4 @@
-import { Service } from "egg";
+import { Service } from 'egg';
 
 export default class Cases extends Service {
 
