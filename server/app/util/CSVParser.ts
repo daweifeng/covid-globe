291c9a7164485bf891341e27373302a6d523eec4
Dawei Feng <imdavidf@gmail.com>
2021-03-01 15:04:19 -0800

    Update csv parser

--- server/app/util/CSVParser.ts
+++ server/app/util/CSVParser.ts
@@ -19,10 +19,35 @@ export class CSV {
 }
 
 export class CSVParser {
-  parse(csvTxt: string): CSV {
-    const allLines = csvTxt.split(/\r\n|\n/);
-    const lineArr = allLines.map(line => line.split(','));
 
-    return new CSV(lineArr);
+  // Return array of string values, or NULL if CSV string not well formed.
+  parse(text: string): CSV {
+    const lines = text.split(/\r\n|\n/);
+    const result = lines.map(line => {
+      const re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+      const re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+
+      // Return NULL if input string is not well formed CSV string.
+      if (!re_valid.test(line)) return [];
+
+      const a: string[] = []; // Initialize array to receive values.
+      line.replace(re_value, // "Walk" the string using replace with callback.
+        function(_m0, m1, m2, m3) {
+          // Remove backslash from \' in single quoted values.
+          if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+
+          // Remove backslash from \" in double quoted values.
+          else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+          else if (m3 !== undefined) a.push(m3);
+          return ''; // Return empty string.
+        });
+
+      // Handle special case of empty last value.
+      if (/,\s*$/.test(line)) a.push('');
+
+      return a;
+    });
+
+    return new CSV(result);
   }
 }
