dc00d665fbe0e43eaf54235bff1f0ccab2672cb7
Dawei Feng <imdavidf@gmail.com>
2021-03-08 14:32:24 -0800

    Update db every 3 hours

--- server/app/schedule/UpdateData.ts
+++ server/app/schedule/UpdateData.ts
@@ -3,7 +3,7 @@ import { Subscription } from 'egg';
 export default class UpdateData extends Subscription {
   static get schedule() {
     return {
-      cron: '0 0 */24 * * *',
+      cron: '0 0 */3 * * *',
       cronOptions: {
         currentDate: new Date('Fri, 10 Jul 2020 00:00:01 UTC'),
       },
